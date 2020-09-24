1f30e312fb4fbcea3b578dc8745f7f11ac9408e2
包涵 <1004578954@qq.com>
2020-09-24 13:03:22 +0800

    1.0.19update

--- Assets/sim-ui/Scripts/TransformData.cs
+++ Assets/sim-ui/Scripts/TransformData.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
-using Assets.Scripts.simai;
-
-namespace Assets.Scripts.SimuUI
+﻿namespace Assets.Scripts.SimuUI
 {
     //public class Vec3
     //{
